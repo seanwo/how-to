c165cd03fd43aa5f9d700446e78c1c02c844d354
seanwo <seanwo@gmail.com>
2019-09-02 07:52:29 -0500

    Update restartqbittorrent.sh

--- ubuntu-mediabox/restartqbittorrent.sh
+++ ubuntu-mediabox/restartqbittorrent.sh
@@ -4,6 +4,6 @@
 /bin/sleep 5s
 /usr/bin/pkill -9 qbittorrent
 /bin/sleep 5s
-env DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/qbittorrent >/dev/null 2>&1
+env DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/qbittorrent >/dev/null 2>&1 &
 
 exit 0
