99f49f8d095b58ab92b3bd0e0810f9a4ce249500
seanwo <seanwo@gmail.com>
2022-11-23 18:46:52 -0600

    Create beautify.sh

---
+++ pinas/beautify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#cleanup resource forks and desktop services store files in the media folders
+
+declare -a extensions=("._*" ".DS_Store")
+declare -a folders=("/srv/mergerfs/mergerfs-pool1")
+for f in "${folders[@]}"
+do
+    for i in "${extensions[@]}"
+    do
+        find $f -type f -name "$i" -exec rm {} \;
+    done
+    find $f -type d -empty -delete
+done
+
+#cleanup extra files in video content folders
+
+declare -a extensions=("*.exe" "*.db" "*.jpg" "*.txt")
+declare -a folders=("/srv/mergerfs/mergerfs-pool1/movies" "/srv/mergerfs/mergerfs-pool1/shows")
+for f in "${folders[@]}"
+do
+    for i in "${extensions[@]}"
+    do
+        find $f -type f -name "$i" -exec rm {} \;
+    done
+    find $f -type d -empty -delete
+done
